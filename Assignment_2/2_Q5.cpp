0f392d7d3b639e2c008a08a2f9b90570cce838e9
Aanandi10 <anandibanerjee2004@gmail.com>
2024-10-03 19:41:04 +0530

    Update 2_Q5.cpp

--- Assignment_2/2_Q5.cpp
+++ Assignment_2/2_Q5.cpp
@@ -1,19 +1,21 @@
-/*Output Explanation:  The function f1 is called with the array m and dim1 equal to 3.
-The function loops over each element of the 2D array and increments each elements by 2.
-The nested loop iterates over all the elements of the array, adding 2 by each: like the
-element at m[0][0] changes from 1 to 3 
-
-The output will be:
-3 4 5 6 7
-13 14 15 16 17
-23 24 25 26 27 */
-
-/* Yes It is Valid to pass a 2D array to a function without specifying the first dimension because
-the first dimension is not needed for the compiler to access the array elements correctly.
-The compiler only needs the sizes of the subsequent dimensions to calculate the corect memory
-addresses when indexing into the array */
-
 #include <iostream>
+
+/*
+The following program increments each element of the 2D array `m` by 2.
+*/
+
+/*
+In C++, it is possible to pass a 2D array to a function without specifying the number of rows.
+However, the number of columns must be defined to allow the compiler to correctly access
+the elements of the array. When passing a 2D array to a function in C++, the size of the second dimension (number of columns)
+must be specified because the compiler needs this information to compute the memory location of
+each element correctly. The number of rows (first dimension) can remain unspecified, as the column size
+is sufficient for the compiler to determine the position of each element.
+*/
+
+
+
+
 
 void f1(int m[][5], int dim1) {
     for(unsigned int i = 0; i < dim1; i++)
