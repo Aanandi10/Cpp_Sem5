74ec7c979b194a775b2505687cdc191481b2fc1b
Aanandi10 <anandibanerjee2004@gmail.com>
2024-10-04 02:38:14 +0530

    Update 4_Q6.cpp

--- Assignment_4/4_Q6.cpp
+++ Assignment_4/4_Q6.cpp
@@ -2,17 +2,16 @@
 #include <iomanip>  // For formatting output
 using namespace std;
 
-struct Month {
+struct Month 
+{
     const char* name;
     int days;
 };
 
-int main() {
+int main()
+{
     // First method: using arrays for month names and days
-    const char* monthNames[] = {
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    };
+    const char* monthNames[] = { "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" };
     int monthDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     // Formatting table headers
@@ -21,19 +20,17 @@ int main() {
     cout << "---------------------------------" << endl;
 
     // Loop through the arrays and print month names with days
-    for (int i = 0; i < 12; i++) {
-        cout << left << setw(15) << monthNames[i] << setw(10) << monthDays[i] << endl;
-    }
+    for (int i = 0; i < 12; i++)
+        {
+           cout << left << setw(15) << monthNames[i] << setw(10) << monthDays[i] << endl;
+         }
 
     // Spacer line for aesthetic purposes
     cout << "\n";
 
     // Second method: using an array of structures
-    Month months[] = {
-        {"January", 31}, {"February", 28}, {"March", 31}, {"April", 30},
-        {"May", 31}, {"June", 30}, {"July", 31}, {"August", 31},
-        {"September", 30}, {"October", 31}, {"November", 30}, {"December", 31}
-    };
+    Month months[] = { {"January", 31}, {"February", 28}, {"March", 31}, {"April", 30},{"May", 31}, {"June", 30}, {"July", 31}, {"August", 31},
+        {"September", 30}, {"October", 31}, {"November", 30}, {"December", 31}};
 
     // Formatting table headers for the structure-based method
     cout << "Month Table using Array of Structure of Months" << endl;
@@ -41,7 +38,8 @@ int main() {
     cout << "---------------------------------" << endl;
 
     // Loop through the array of structures and print the data
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 12; i++)
+    {
         cout << left << setw(15) << months[i].name << setw(10) << months[i].days << endl;
     }
 
