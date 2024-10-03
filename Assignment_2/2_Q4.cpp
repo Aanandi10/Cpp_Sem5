fe26763167a097faff11673154d46e3bdd306b57
Aanandi10 <anandibanerjee2004@gmail.com>
2024-10-03 12:19:10 +0530

    Update 2_Q4.cpp

--- Assignment_2/2_Q4.cpp
+++ Assignment_2/2_Q4.cpp
@@ -1,10 +1,27 @@
-/*No the declaration "void func(char= '*', int =45);" is not valid in c++
+#include <iostream>
+using namespace std;
 
-in C++,when defining function paramter with default values, you must specify the 
-data type for each paramter. The correct way to declare this fuction would be:*/
+/*
+This is a valid function declaration in C++ where parameters are assigned default values.
+If no arguments are provided when calling the function, the char parameter will default to '*'
+and the int parameter will default to 45.
+*/
 
-void func(char ch= '*', int=45);
+void func(char = '*', int = 45);
 
-/*If we declare this way the compiler wil generate a syntex error. The compiler will complain
-that declaration is not valid because it expects a type before the parameter name. we will see
-an error message indicating a "type name is not allowed"*/
+/*
+When the function is called without any arguments, both parameters take their default values.
+If you pass only one argument, it will be assigned to the first parameter (char), and the second 
+parameter (int) will retain its default value.
+If both arguments are provided, the defaults are bypassed and the given values are used.
+*/
+void func(char c, int num) {
+    cout << "Character: " << c << " | Number: " << num << endl;
+}
+
+int main() {
+    func();          // Both parameters use their default values: '*', 45
+    func('#');       // First parameter is '#', second uses default 45
+    func('#', 10);   // Both parameters use provided values: '#', 10
+    return 0;
+}
