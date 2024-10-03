f9127c6f7df6606095d1df032899283be31bedb5
Aanandi10 <anandibanerjee2004@gmail.com>
2024-10-03 19:49:05 +0530

    Update 2_Q6.cpp

--- Assignment_2/2_Q6.cpp
+++ Assignment_2/2_Q6.cpp
@@ -10,16 +10,14 @@ void sort1(int *a, int n) {
 }
 
 // Selection sorting algorithm for strings
-void sort2(const char *a[], int n) {
-    for (unsigned int i = 0; i < n - 1; i++) {
+void sort2(char *a[], int n) {
+    for (unsigned int i = 0; i < n; i++) {
         unsigned int minIndex = i;
         for (unsigned int j = i + 1; j < n; j++) {
-            // Compare strings using strcmp
-            if (strcmp(a[j], a[minIndex]) < 0) {
+            if (std::strcmp(a[j], a[minIndex]) < 0) {
                 minIndex = j;
             }
         }
-        // Swap the found minimum string with the first string
         if (minIndex != i) {
             std::swap(a[i], a[minIndex]);
         }
@@ -28,25 +26,23 @@ void sort2(const char *a[], int n) {
 
 int main() {
     int a[] = {-2, 3, 8, 10, 7, 56, 90};
-    const char* str[] = {"hello", "world", "iacs", "raining"};
+    char* str[] = {"hello", "world", "iacs", "raining"};
 
     // Sorting integers
     void (*sortInt)(int *, int);
     sortInt = &sort1;
-    sortInt(a, 7); // call via function pointer
-
+    sortInt(a, 7); // Call via function pointer
     for (unsigned int i = 0; i < 7; i++)
         std::cout << a[i] << " ";
     std::cout << "\n";
-    
-    // Sorting strings
-    void (*sortString)(const char*[], int);
-    sortString = &sort2;
-    sortString(str, 4); // call via function pointer
 
+    // Sorting strings
+    void (*sortString)(char*[], int);
+    sortString = &sort2;
+    sortString(str, 4); // Call via function pointer
     for (unsigned int i = 0; i < 4; i++)
         std::cout << str[i] << " ";
     std::cout << "\n";
-    
+
     return 0;
-}  
+}
